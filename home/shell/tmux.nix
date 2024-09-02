65d6789355cd6ef7b73a2b5c9d2f11fd591c085a
Nicholas Solichin <nrsolichin@gmail.com>
2024-09-02 22:08:15 +0000

    [tmux] Bind r to allow reloading tmux config

--- home/shell/tmux.nix
+++ home/shell/tmux.nix
@@ -19,6 +19,9 @@
         set -g status on
         set -g mouse on
 
+        unbind r
+        bind r source-file ~/.config/tmux/tmux.conf
+
         # Catppuccin options
         set -g @catppuccin_host 'on'
         set -g @catppuccin_window_tabs_enabled 'on'
